5db79e13b6f7f4928ae3f7c94c033cde0f93b061
sathesahil <sathesahil2008@gmail.com>
2026-04-15 16:19:27 +0530

    Delete Python Project2 directory

--- Python Project2/run_CheetahType.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "TYPEX" "C:\Program Files\Python314\pythonw.exe" "c:\Users\Admin\OneDrive\Desktop\Sahil Coding\Semester 4\FrontEnd\Python Project2\typeSpeedChecker.pyw"
--- Python Project2/typeSpeedChecker.pyw
+++
@@ -1,5 +0,0 @@
-from typeSpeedChecker import main
-
-
-if __name__ == "__main__":
-    main()
